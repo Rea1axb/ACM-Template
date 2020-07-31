e62a37b6a5042612a9e1d8e136128912e52e48f5
Rea1axb <56550788+Rea1axb@users.noreply.github.com>
2020-07-31 19:31:47 +0800

    Delete gcd+lcm.cpp

--- 数论/GCD/gcd+lcm.cpp
+++
@@ -1,9 +0,0 @@
-//先求n！逆元
-for(int i=n-1;i>=1;i--)
-{
- 	inv[i]=inv[i+1]*(i+1)%mod;ll gcd(ll a,ll b){
-	return b?gcd(b,a%b):a;
-}//最大公约      //建议直接__gcd()
-ll lcm(ll a,ll b){
-	return a*b/gcd(a,b);//可能会爆，快速乘
-}//最大公倍
