52d7ccb0f2898450c99610464b7d5b052e731f4f
Rea1axb <1527039517@qq.com>
2020-09-09 15:12:08 +0800

    update stk queue

--- 数据结构/单调栈单调队列/单调栈.cpp
+++ 数据结构/单调栈单调队列/单调栈.cpp
@@ -1,7 +1,19 @@
-int s[N]，top=0;
-for(int i=1;i<=n;i++)
-{
-	while(top&&a[s[top]]>a[i]) top--;
-	if(s[top]==a[i]) s[top]=i;//去重复操作，视情况而定
-	s[++top]=i;
+int s[N]，top = 0;
+for(int i = 1; i <= n; i++) {
+    while(top && a[s[top]] > a[i])
+        top--;
+    if(a[s[top]] == a[i])
+        s[top] = i; //去重复操作，视情况而定
+    else
+        s[++top] = i;
+}
+
+stack<int> stk;
+for (int i = 1; i <= n; i++) {
+    while (!stk.empty() && a[stk.top()] > a[i]) {
+        stk.pop();
+    }
+    if (!stk.empty() && a[stk.top()] == a[i])
+        stk.pop();
+    stk.push(i);
 }
--- 数据结构/单调栈单调队列/单调队列.cpp
+++ 数据结构/单调栈单调队列/单调队列.cpp
@@ -1,9 +1,23 @@
-int l=0,r=1;
+//求 [i - m + 1, i]的最小值
+int l = 0, r = 1;
 int q[N];
-for(int i=1;i<=n;i++)
-{
-	while(l<=r&&a[q[r]]>a[i]) r--;
-	q[++r]=i;
-	while(l<=r&&q[l]<i-m+1) l++;//判断条件看情况
-	ans[i]=a[q[l]];
+for(int i = 1; i <= n; i++) {
+    while(l <= r && a[q[r]] > a[i])
+        r--;
+    q[++r] = i;
+    while(l <= r && q[l] < i - m + 1)
+        l++;//判断条件看情况
+    ans[i] = a[q[l]];
+}
+
+deque<int> q;
+for (int i = 1; i <= n; i++) {
+    while (!q.empty() && a[q.back()] > a[i]) {
+        q.pop_back();
+    }
+    q.push_back(i);
+    while (!q.empty() && q.front() < i - m + 1) {
+        q.pop_front();
+    }
+    ans[i] = a[q.front()];
 }
