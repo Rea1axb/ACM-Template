4506b4698d4a8fe6ce23c1c6b8761ee93a4eb613
Rea1axb <1527039517@qq.com>
2020-09-18 14:13:20 +0800

    update gauss

--- 数学/高斯消元.cpp
+++ 数学/高斯消元.cpp
@@ -30,3 +30,30 @@ vec gauss_jordan(const mat& A, const vec& b) {
     for (int i = 0; i < n; i++) x[i] = B[i][n];
     return x;
 }
+
+//解异或方程组
+vec gauss_jordan(const mat &A, const vec &b) {
+    int n = A.size();
+    mat B(n, vec(n + 1));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            B[i][j] = A[i][j];
+    for (int i = 0; i < n; i++) B[i][n] = b[i];
+    for (int i = 0; i < n; i++) {
+        int pivot = i;
+        for (int j = i; j < n; j++) {
+            if (abs(B[j][i]) > abs(B[pivot][i])) pivot = j;
+        }
+        swap(B[i], B[pivot]);
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                if (!B[j][i]) continue;
+                for (int k = i; k <= n; k++)
+                    B[j][k] ^= B[i][k];
+            }
+        }
+    }
+    vec x(n);
+    for (int i = 0; i < n; i++) x[i] = B[i][n];
+    return x;
+}
