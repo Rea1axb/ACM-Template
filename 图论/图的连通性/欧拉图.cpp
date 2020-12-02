9af2f24bf66e288228f6030ce5c9c3c663079495
Rea1axb <1527039517@qq.com>
2020-12-02 21:09:15 +0800

    update euler

---
+++ 图论/图的连通性/欧拉图.cpp
@@ -0,0 +1,25 @@
+//欧拉回路 or 欧拉通路
+
+//无向图
+vector<int> ans;
+visedge[MAXM * 2];
+void dfs(int u) {
+    for (int i = first[u]; i != -1; i = e[i].next) {
+        if (visedge[i]) continue;
+        visedge[i] = visedge[i ^ 1] = 1;
+        dfs(e[i].v);
+        ans.push_back(i);
+    }
+}
+
+//有向图
+vector<int> ans;
+visedge[MAXM];
+void dfs(int u) {
+    for (int i = first[u]; i != -1; i = e[i].next) {
+        if (visedge[i]) continue;
+        visedge[i] = 1;
+        dfs(e[i].v);
+        ans.push_back(i);
+    }
+}
