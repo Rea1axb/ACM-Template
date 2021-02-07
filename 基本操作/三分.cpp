1ffa5c2f84924e0649bd23014f351ab21aa4603c
Rea1axb <1527039517@qq.com>
2021-02-07 19:25:36 +0800

    update 三分

--- 基本操作/三分.cpp
+++ 基本操作/三分.cpp
@@ -1,7 +1,7 @@
 //凸函数求最大值
 ll ans = 0;
 while (l < r) { //zbyk
-    int lmid = (l + r) >> 1, rmid = (lmid + r) >> 1;
+    ll lmid = (l + r) >> 1, rmid = (lmid + r) >> 1;
     ll lans = cal(lmid), rans = cal(rmid);
     if (lans >= rans) {
         r = rmid;
@@ -16,7 +16,7 @@ while (l < r) { //zbyk
 //凹函数求最小值
 ll ans = INF;
 while (l < r) {
-    int lmid = (l + r) >> 1, rmid = (lmid + r) >> 1;
+    ll lmid = (l + r) >> 1, rmid = (lmid + r) >> 1;
     ll lans = cal(lmid), rans = cal(rmid);
     if (lans <= rans) {
         r = rmid;
