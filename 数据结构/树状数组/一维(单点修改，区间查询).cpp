47f9a7136f11feb8978d8c892db13aceb15432ce
Rea1axb <1527039517@qq.com>
2021-02-11 21:09:59 +0800

    树状数组后缀最值

--- 数据结构/树状数组/一维(单点修改，区间查询).cpp
+++ 数据结构/树状数组/一维(单点修改，区间查询).cpp
@@ -41,17 +41,26 @@ BIT::change(i, k);
 ans = query(l, r);//多组数据时要考虑其他组测试数据的影响
 
 //求后缀和只需把updata和getsum取反，二维同理
-void updata(int i, int k) {
+
+//求后缀最大值
+
+void updata(int i, ll k) {
     while (i > 0) {
-        c[i] += k;
+        c[i] = max(c[i], k);
         i -= lowbit(i);
     }
 }
 ll getsum(int i) {
-    ll res = 0;
+    ll res = -INF;
     while (i <= n) {
-        res += c[i];
+        res = max(res, c[i]);
         i += lowbit(i);
     }
     return res;
+}
+void init(int _n) {
+    n = _n;
+    for (int i = 1; i <= n; i++) {
+        c[i] = -INF;
+    }
 }
