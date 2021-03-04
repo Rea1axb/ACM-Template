016f9e216cb67ff8bb641eba1d7906118bea043c
Rea1axb <1527039517@qq.com>
2021-03-04 16:10:01 +0800

    最大流唯一性和最小割唯一性

--- 图论/网络流/最大流(Dinic).cpp
+++ 图论/网络流/最大流(Dinic).cpp
@@ -78,30 +78,64 @@ namespace MaxFlow {
             flow += dfs(s, INF);
         }
     }
-//    判断最大流的唯一性
-//    int vis[MAXN];
-//    int judge(int u, int fa, int rt) {
-//        vis[u] = 1;
-//        for (int i = first[u]; i != -1; i = e[i].next) {
-//            int v = e[i].v;
-//            if (v == fa || !e[i].f) continue;
-//            if (v == rt) return 0;
-//            if (!vis[v]) {
-//                int ok = judge(v, u, rt);
-//                if (!ok) return 0;
-//            }
-//        }
-//        return 1;
-//    }
-//
-//    bool is_unique() {
-//        for (int i = 1; i <= n; i++) {
-//            fill(vis, vis + n + 1, 0);
-//            int ok = judge(i, -1, i);
-//            if (!ok) return 0;
-//        }
-//        return 1;
-//    }
+
+    int used[MAXN];
+    int judge(int u, int fa, int rt) {
+        used[u] = 1;
+        for (int i = first[u]; i != -1; i = e[i].next) {
+            int v = e[i].v;
+            if (v == fa || !e[i].f) continue;
+            if (v == rt) return 0;
+            if (!used[v]) {
+                int ok = judge(v, u, rt);
+                if (!ok) return 0;
+            }
+        }
+        return 1;
+    }
+    bool maxflow_unique() {
+        for (int i = 1; i <= n; i++) {
+            fill(used, used + n + 1, 0);
+            int ok = judge(i, -1, i);
+            if (!ok) return 0;
+        }
+        return 1;
+    }
+
+    bool mincost_unique() {
+        queue<int> q1, q2;
+        vector<int> vis(n + 1);
+        q1.push(s);
+        vis[s] = 1;
+        while (!q1.empty()) {
+            int cur = q1.front();
+            q1.pop();
+            for (int i = first[cur]; i != -1; i = e[i].next) {
+                int v = e[i].v;
+                if (!vis[v] && e[i].f) {
+                    vis[v] = 1;
+                    q1.push(v);
+                }
+            }
+        }
+        q2.push(t);
+        vis[t] = 1;
+        while (!q2.empty()) {
+            int cur = q2.front();
+            q2.pop();
+            for (int i = first[cur]; i != -1; i = e[i].next) {
+                int v = e[i].v;
+                if (!vis[v] && e[i ^ 1].f) {
+                    vis[v] = 1;
+                    q2.push(v);
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) return 0;
+        }
+        return 1;
+    }
 }
 
 //初始化
@@ -117,3 +151,9 @@ flow = MCMF::flow;
 
 //反向边有流量的正向边为答案中有流量的边
 //判断一条边是否是正向边可以用边的编号的奇偶性判断
+
+//最大流唯一性（如果残余网络中是否存在长度大于等于3的环，则最大流不唯一）
+maxflow_unique = MaxFlow::maxflow_unique();
+
+//最小割唯一性（如果在参与网络中存在既不属于S，也不属于T的点，则最小割不唯一）
+mincost_unique = MaxFlow::mincost_unique();
