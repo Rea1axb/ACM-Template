d3e85541bccc9bd862fc883a2d1862519e8867b0
Rea1axb <1527039517@qq.com>
2021-03-05 10:20:14 +0800

    欧拉图

--- 图论/图的连通性/欧拉图.cpp
+++ 图论/图的连通性/欧拉图.cpp
@@ -1,25 +1,25 @@
 //欧拉回路 or 欧拉通路
 
 //无向图
-vector<int> ans;
+stack<int> ans;
 int visedge[MAXM * 2];
 void dfs(int u) {
     for (int i = first[u]; i != -1; i = e[i].next) {
         if (visedge[i]) continue;
         visedge[i] = visedge[i ^ 1] = 1;
         dfs(e[i].v);
-        ans.push_back(i);
+        ans.push(i);
     }
 }
 
 //有向图
-vector<int> ans;
+stack<int> ans;
 int visedge[MAXM];
 void dfs(int u) {
     for (int i = first[u]; i != -1; i = e[i].next) {
         if (visedge[i]) continue;
         visedge[i] = 1;
         dfs(e[i].v);
-        ans.push_back(i);
+        ans.push(i);
     }
 }
