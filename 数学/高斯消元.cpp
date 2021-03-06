9f1d7d99283a808655c874f3fb38c6fc4835c29a
Rea1axb <1527039517@qq.com>
2021-03-06 11:48:42 +0800

    高斯消元 异或方程组

--- 数学/高斯消元.cpp
+++ 数学/高斯消元.cpp
@@ -32,6 +32,8 @@ vec gauss_jordan(const mat& A, const vec& b) {
 }
 
 //解异或方程组
+bool ok = 1; //有无解
+int cnt = 0; //自由元个数
 vec gauss_jordan(const mat &A, const vec &b) {
     int n = A.size();
     mat B(n, vec(n + 1));
@@ -39,21 +41,89 @@ vec gauss_jordan(const mat &A, const vec &b) {
         for (int j = 0; j < n; j++)
             B[i][j] = A[i][j];
     for (int i = 0; i < n; i++) B[i][n] = b[i];
-    for (int i = 0; i < n; i++) {
-        int pivot = i;
-        for (int j = i; j < n; j++) {
-            if (abs(B[j][i]) > abs(B[pivot][i])) pivot = j;
+    for (int r = 0, c = 0; r < n && c < n; r++, c++) {
+        int pivot = r;
+        for (int i = r + 1; i < n; i++) {
+            if (abs(B[i][c]) > abs(B[pivot][c])) pivot = i;
         }
-        swap(B[i], B[pivot]);
-        for (int j = 0; j < n; j++) {
-            if (i != j) {
-                if (!B[j][i]) continue;
-                for (int k = i; k <= n; k++)
-                    B[j][k] ^= B[i][k];
+        swap(B[r], B[pivot]);
+        if (B[r][c] == 0) {
+            r--;
+            continue;
+        }
+        for (int i = r + 1; i < n; i++) {
+            if (!B[i][c]) continue;
+            for (int j = c; j <= n; j++) {
+                B[i][j] ^= B[r][j];
             }
         }
     }
     vec x(n);
-    for (int i = 0; i < n; i++) x[i] = B[i][n];
+    for (int i = 0; i < n; i++) {
+        int pos = -1;
+        for (int j = 0; j < n; j++) {
+            if (B[i][j]) {
+                pos = j;
+                break;
+            }
+        }
+        if (pos == -1 && B[i][n] == 1) { //无解
+            ok = 0;
+            return vec();
+        }
+        if (pos == -1 && B[i][n] == 0) {
+            cnt++;
+            continue;
+        }
+        x[pos] = B[i][n];
+    }
     return x;
+}
+
+//解01异或方程组
+typedef bitset<MAXL> bi;
+vector<bi> X(n + 1);
+for (int i = 1; i <= n; i++) {
+    for (int j = 1 j <= n; j++) {
+        x[i][j] = A[i][j];
+    }
+    x[i][0] = b[i];
+}
+bool ok = 1; //有无解
+int cnt = 0; //自由元数量
+vec gauss_jordan(vector<bi> X) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++)
+            if (X[j].to_string() > X[i].to_string()) swap(X[i], X[j]);
+        if (X[i] == 0) {
+            //这个和后面都是自由元
+            cnt = n - i + 1;
+            break;
+        }
+        if (X[i] == 1) {
+            //无解
+            ok = 0;
+            return vec();
+        }
+        for (int k = n; k > 0; k--) {
+            if (X[i].test(k)) {
+                for (int j = 1; j <= n; j++) {
+                    if (i != j && X[j].test(k)) {
+                        X[j] ^= X[i];
+                    }
+                }
+                break;
+            }
+        }
+    }
+    vec res(n + 1);
+    for (int i = 1; i <= n - cnt; i++) {
+        for (int k = n; k > 0; k--) {
+            if (X[i].test(k)) {
+                res[k] = X[i][0];
+                break;
+            }
+        }
+    }
+    return res;
 }
