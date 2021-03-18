0e6af0f128338f4e83281546d4417d22d0c14650
Rea1axb <1527039517@qq.com>
2021-03-18 20:45:00 +0800

    2-SAT

---
+++ 图论/图的连通性/2-SAT.cpp
@@ -0,0 +1,119 @@
+/*
+题面：有n个气球，每个气球是红色或者蓝色，有k个人，每个人选择 3 个气球并猜气球的颜色。
+一个人猜对两个气球或以上的颜色就可以获得奖品。
+判断是否存在一个气球的着色方案使得每个人都能领奖，若有则输出一种气球的着色方案。
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int MAXN = 1e5 + 10;
+const int MAXM = 1e5 + 10;
+struct edge {
+    int u, v;
+    int next;
+}e[MAXM * 2];
+int n, idx, first[MAXN];
+void add(int u, int v) {
+    e[idx].u = u;
+    e[idx].v = v;
+    e[idx].next = first[u];
+    first[u] = idx++;
+}
+void init() {
+    idx = 0;
+    fill(first, first + n * 2 + 1, -1); //点数两倍
+}
+namespace Tarjan {
+    int dfn[MAXN];
+    int low[MAXN];
+    stack<int> stk;
+    int vis[MAXN];
+    int n, cnt, resnum[MAXN];
+    vector<int> res[MAXN];
+    int times;
+
+    void dfs(int u, int fa) {
+        dfn[u] = low[u] = ++times;
+        stk.push(u);
+        vis[u] = 1;
+        for (int i = first[u]; i != -1; i = e[i].next) {
+            int v = e[i].v;
+            if (!dfn[v]) {
+                dfs(v, u);
+                low[u] = min(low[u], low[v]);
+            } else if (vis[v]) {
+                low[u] = min(low[u], dfn[v]);
+            }
+        }
+        if (low[u] == dfn[u]) {
+            cnt++;
+            res[cnt].clear();
+            while (!stk.empty()) {
+                int cur = stk.top();
+                stk.pop();
+                resnum[cur] = cnt;
+                res[cnt].push_back(cur);
+                vis[cur] = 0;
+                if (cur == u) break;
+            }
+        }
+    }
+    void init(int _n) {
+        n = _n;
+        fill(dfn, dfn + n + 1, 0);
+        fill(resnum, resnum + n + 1, 0);
+        fill(vis, vis + n + 1, 0);
+        while (!stk.empty()) stk.pop();
+        cnt = 0;
+        times = 0;
+    }
+
+    void solve() {
+        for (int i = 1; i <= n; i++) {
+            if (!dfn[i])
+                dfs(i, -1);
+        }
+        bool flag = 1;
+        for (int i = 1; i <= n / 2; i++) {
+            if (resnum[i] == resnum[i + n / 2]) { //无可行解
+                flag = 0;
+                break;
+            }
+        }
+        if (!flag) {
+            printf("-1\n");
+            return;
+        }
+        for (int i = 1; i <= n / 2; i++) {
+            if (resnum[i] < resnum[i + n / 2]) { //选择连通块编号小的点
+                printf("R");
+            } else {
+                printf("B");
+            }
+        }
+    }
+}
+int main() {
+    int k;
+    scanf("%d%d", &n, &k);
+    init();
+    for (int i = 1; i <= k; i++) {
+        int a, b, c;
+        char op1, op2, op3;
+        scanf("%d %c %d %c %d %c", &a, &op1, &b, &op2, &c, &op3);
+        int val1 = (op1 == 'B' ? 1 : 0); //val为1表示气球是B颜色
+        int val2 = (op2 == 'B' ? 1 : 0);
+        int val3 = (op3 == 'B' ? 1 : 0);
+        add(a + (val1 ^ 1) * n, b + val2 * n); //如果a猜错了，那么b,c猜对
+        add(a + (val1 ^ 1) * n, c + val3 * n);
+        add(b + (val2 ^ 1) * n, a + val1 * n); //如果b猜错了，那么a,c猜对
+        add(b + (val2 ^ 1) * n, c + val3 * n);
+        add(c + (val3 ^ 1) * n, a + val1 * n); //如果c猜错了，那么a,b猜对
+        add(c + (val3 ^ 1) * n, b + val2 * n);
+    }
+    Tarjan::init(n * 2);
+    Tarjan::solve();
+    return 0;
+}
